1877f3a622e634dcf122cb652ca563e3f6ed2386
denisramosesti-ung <denisramosesti@gmail.com>
2026-02-28 13:24:03 -0300

    Refactor padronDB.js for better DB management
    Refactor database functions to use openDB and improve object store handling.

--- src/utils/padronDB.js
+++ src/utils/padronDB.js
@@ -1,59 +1,42 @@
 const DB_NAME = "padronDB";
 const STORE_NAME = "padronStore";
-const DB_VERSION = 2;
 
-export const initDB = () => {
+function openDB() {
   return new Promise((resolve, reject) => {
-    const request = indexedDB.open(DB_NAME, DB_VERSION);
+    const request = indexedDB.open(DB_NAME, 1);
 
-    request.onupgradeneeded = (event) => {
+    request.onupgradeneeded = function (event) {
       const db = event.target.result;
       if (!db.objectStoreNames.contains(STORE_NAME)) {
-        db.createObjectStore(STORE_NAME);
+        db.createObjectStore(STORE_NAME, { keyPath: "ci" });
       }
     };
 
     request.onsuccess = () => resolve(request.result);
     request.onerror = () => reject(request.error);
   });
-};
+}
 
-export const savePadron = async (padron) => {
-  const db = await initDB();
+export async function savePadron(padron) {
+  const db = await openDB();
   const tx = db.transaction(STORE_NAME, "readwrite");
   const store = tx.objectStore(STORE_NAME);
 
-  store.put(padron, "fullPadron");
+  for (const persona of padron) {
+    store.put(persona);
+  }
 
-  return new Promise((resolve, reject) => {
-    tx.oncomplete = () => resolve(true);
-    tx.onerror = () => reject(tx.error);
-    tx.onabort = () => reject(tx.error);
-  });
-};
+  return tx.complete;
+}
 
-export const getAllPadron = async () => {
-  const db = await initDB();
+export async function getAllPadron() {
+  const db = await openDB();
   const tx = db.transaction(STORE_NAME, "readonly");
   const store = tx.objectStore(STORE_NAME);
 
   return new Promise((resolve, reject) => {
-    const request = store.get("fullPadron");
-    request.onsuccess = () => resolve(request.result || []);
+    const request = store.getAll();
+    request.onsuccess = () => resolve(request.result);
     request.onerror = () => reject(request.error);
   });
-};
-
-export const clearPadron = async () => {
-  const db = await initDB();
-  const tx = db.transaction(STORE_NAME, "readwrite");
-  const store = tx.objectStore(STORE_NAME);
-
-  store.delete("fullPadron");
-
-  return new Promise((resolve, reject) => {
-    tx.oncomplete = () => resolve(true);
-    tx.onerror = () => reject(tx.error);
-    tx.onabort = () => reject(tx.error);
-  });
-};
+}
